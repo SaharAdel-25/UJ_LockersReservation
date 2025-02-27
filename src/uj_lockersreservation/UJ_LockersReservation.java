6a5bb29dc605dc2c195cc3376e024844959ae83d
SaharAdel-25 <soeaix@gmail.com>
2025-02-27 22:16:09 +0300

    Update UJ_LockersReservation.java

--- src/uj_lockersreservation/UJ_LockersReservation.java
+++ src/uj_lockersreservation/UJ_LockersReservation.java
@@ -3,12 +3,6 @@ package uj_lockersreservation;
 
 import javax.swing.JFrame;
 
-/*
-Suad Anees Alsaadi 2219146
-Sahar Adel 2219170
-Sara Hamdan 2210190
- */
-
 public class UJ_LockersReservation extends JFrame {
 
     public static void main(String[] args) {
